519a8b59c225489a113a312132ce970dcc551568
Tanveer Khan <kntanveer7@gmail.com>
2026-02-21 12:16:49 +0530

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,12 @@ import { products } from '../data/products';
 import './HomePage.css';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products').then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+        });
+    });
+
     return (
         <>
             <title>Homepage</title>
