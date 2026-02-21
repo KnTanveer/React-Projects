d3944cd4d5289f059528e2e9d1ad206d9ff71e48
Tanveer Khan <kntanveer7@gmail.com>
2026-02-21 13:58:28 +0530

    lift up cart

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,18 +1,27 @@
-import { Routes, Route } from 'react-router'
-import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
-import { OrdersPage } from './pages/OrdersPage'
-import { TrackingPage } from './pages/TrackingPage'
-import { ErrorPage } from './pages/ErrorPage'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
+import { ErrorPage } from './pages/ErrorPage';
 import './App.css'
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items').then((response) => {
+      setCart(response.data);
+    })
+  }, [])
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='checkout' element={<CheckoutPage />} />
+        <Route path='/' element={<HomePage cart={cart} />} />
+        <Route path='checkout' element={<CheckoutPage cart={cart} />} />
         <Route path='orders' element={<OrdersPage />} />
         <Route path='tracking' element={<TrackingPage />} />
         <Route path='*' element={<ErrorPage />} />
